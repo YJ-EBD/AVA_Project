60e71d3d41efad19b5cde40c2fffb39d474ab21c
ABBAS <abbas@ABBASui-MacBookAir.local>
2026-06-16 16:25:36 +0900

    Set iOS export encryption compliance flag

--- Flutter/lib/src/config/app_version.dart
+++ Flutter/lib/src/config/app_version.dart
@@ -3,11 +3,11 @@ class AppVersion {
 
   static const name = String.fromEnvironment(
     'AVA_APP_VERSION',
-    defaultValue: '0.1.302',
+    defaultValue: '0.1.303',
   );
   static const buildNumber = int.fromEnvironment(
     'AVA_BUILD_NUMBER',
-    defaultValue: 1302,
+    defaultValue: 1303,
   );
   static const display = 'ver. $name';
 }
