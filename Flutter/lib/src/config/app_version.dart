422b537d11a4a885a271c2163e988ccb327d50ea
ABBAS <abbas@ABBASui-MacBookAir.local>
2026-06-18 16:40:47 +0900

    Update iOS TestFlight build to 0.1.307

--- Flutter/lib/src/config/app_version.dart
+++ Flutter/lib/src/config/app_version.dart
@@ -3,11 +3,11 @@ class AppVersion {
 
   static const name = String.fromEnvironment(
     'AVA_APP_VERSION',
-    defaultValue: '0.1.306',
+    defaultValue: '0.1.307',
   );
   static const buildNumber = int.fromEnvironment(
     'AVA_BUILD_NUMBER',
-    defaultValue: 1306,
+    defaultValue: 1307,
   );
   static const display = 'ver. $name';
 }
